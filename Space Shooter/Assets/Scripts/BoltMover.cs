7284788f489bd39f22d6c723bf1b674e26e4a3c9
JackTongy <1614362573@qq.com>
2017-09-29 21:33:41 +0800

    space shooter add shot code.

---
+++ Space Shooter/Assets/Scripts/BoltMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltMover : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = speed * transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
