c28fb13c931ae2df13c4ff062465e3fa0e92b263
ty <ty@mrglee.com>
2017-09-29 09:45:18 +0800

    Space shooter project.

---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
+	}
+}
